f2d59c59ca18563a86a3473aa4b3f1d93205ea63
Daniel Jimenez <dj@MacBook-Pro.local>
2020-05-25 10:04:34 -0600

    redux added

--- src/containers/App.js
+++ src/containers/App.js
@@ -39,7 +39,6 @@ class App extends React.Component {
     const {robots} = this.state
     const {searchField, onSearchChange} = this.props
     const filteredRobots = robots.filter(robot => {
-      console.log(searchField)
        return robot.name.toLowerCase().includes(searchField.toLowerCase())
     }) 
     return  !robots.length ? <h1 className="tc">Loading</h1> : (
